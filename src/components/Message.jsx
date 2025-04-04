de281702e39e53d558d3c59515b74dd6c3d688d0
osmanaligungor <o.aligngr3232@gmail.com>
2025-04-04 15:39:54 +0300

    upadated

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,19 +1,26 @@
 import { auth } from "../firebase";
+import generateColor from "../utils/generateColor";
 
 const Message = ({ data }) => {
   // eğer mesajı oturumu açık olan kullanıcı attıysa : sağ tarafta
   if (auth.currentUser.uid === data.author.id) {
-    return <div className="msg-user">{data.text}</div>;
+    return <p className="msg-user">{data.text}</p>;
   }
 
   // eğer mesajı farklı bir kullanıcı attıysa : sol tarafta
   return (
     <div className="msg-other">
+      <img src={data.author.photo} />
       <div>
-        <img src={data.author.photo} />
-        <span>{data.author.name}</span>
+        <span
+          style={{
+            color: generateColor(data.author.id),
+          }}
+        >
+          {data.author.name}
+        </span>
+        <p className="msg-text">{data.text}</p>
       </div>
-      <p className="msg-text">{data.text}</p>
     </div>
   );
 };
